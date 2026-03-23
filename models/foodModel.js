27d20c91901d24176d57b653e2ab232c9f7363e9
yashsolanki12 <yash@ecodesoft.com>
2026-03-23 10:47:12 +0530

    Update the required field server, type while create food & integrate update food API

--- models/foodModel.js
+++ models/foodModel.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-const foodSchema = new mongoose.Schema({ 
-    name: { type: String, required: true },
-    description : { type: String, required: true },
-    price : { type: Number, required: true },
-    image : { type: String, required: true },
-    category : { type: String, required: true },
-    serve : { type: String, required: true },
-    type : { type: String, required: true }
-})
+const foodSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  category: { type: String, required: true },
+  serve: { type: String, required: false },
+  type: { type: String, required: false },
+});
 
 const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
 
